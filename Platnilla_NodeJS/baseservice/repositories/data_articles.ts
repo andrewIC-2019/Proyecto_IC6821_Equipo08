5b1e66b830d18a2c08bcfed60ea926954a9c28f8
andrewIC-2019 <61808125+andrewIC-2019@users.noreply.github.com>
2022-05-02 13:51:51 -0600

    Plantilla NodeJS

---
+++ Platnilla_NodeJS/baseservice/repositories/data_articles.ts
@@ -0,0 +1,21 @@
+import { Logger } from '../common'
+
+/*
+VENGO DE LOS CONTROLADORES
+*/
+
+//aqui vendria lo de sql server
+//lo basico para devolver datos
+export class articles_data {
+    private log: Logger;
+
+    public constructor()
+    {
+    }
+
+    public getAllArticles() : Promise<any>
+    {
+        return null;
+    }
+
+}
